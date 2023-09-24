b26252340ddc1871778117ff4038514b3c929d67
Jason Cassella <jasoncassella1@gmail.com>
2023-09-24 11:36:12 -0400

    whoops dont need that

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ export default function App() {
 				foot on the moon or when rovers were sent to roam around on Mars.
 			</TextExpander>
 
-			<TextExpander expanded={true} className='box'>
+			<TextExpander>
 				Space missions have given us incredible insights into our universe and
 				have inspired future generations to keep reaching for the stars. Space
 				travel is a pretty cool thing to think about. Who knows what we'll
